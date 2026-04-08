607b3ad24c2c9c83d548dce4cb87c17e7c477472
ylcharan <ylcharan99@gmail.com>
2026-04-08 22:42:39 +0530

    production temporarliy down

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
 import { NextResponse } from "next/server";
 
 const MAINTENANCE_MODE = true;
 
-export default function middleware(req: Request) {
+export default clerkMiddleware((auth, req) => {
   if (MAINTENANCE_MODE) {
     return new NextResponse("Site temporarily down", {
       status: 503,
@@ -12,13 +11,11 @@ export default function middleware(req: Request) {
   }
 
   return NextResponse.next();
-}
-export const proxy = clerkMiddleware(); // proxy.ts
+});
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
